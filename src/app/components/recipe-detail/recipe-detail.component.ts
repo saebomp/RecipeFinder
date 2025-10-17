ceded0fad86a29a5d02ee6cf4d7ddb12d437997d
SAEBOMPAK <saebompak@gmail.com>
2025-10-16 21:42:58 -0700

    add more details to recipe detail page

--- src/app/components/recipe-detail/recipe-detail.component.ts
+++ src/app/components/recipe-detail/recipe-detail.component.ts
@@ -13,8 +13,9 @@ import { faCircleArrowLeft } from '@fortawesome/free-solid-svg-icons';
   styleUrl: './recipe-detail.component.css',
 })
 export class RecipeDetailComponent implements OnInit {
-  recipe: any;
+  recipes: any;
   faCircleArrowLeft = faCircleArrowLeft;
+  ingredients: { ingredient: string; measure: string }[] = [];
 
   constructor(private route: ActivatedRoute, private apiService: ApiService) {}
 
@@ -22,7 +23,24 @@ export class RecipeDetailComponent implements OnInit {
     const id = this.route.snapshot.paramMap.get('id');
     if (id) {
       this.apiService.getRecipeById(id).subscribe((data: any) => {
-        this.recipe = data.meals[0];
+        this.recipes = data.meals;
+        console.log(this.recipes);
+
+        const meal = this.recipes[0]; // meals 배열에서 첫 번째 요리만 사용
+        this.ingredients = Object.keys(meal)
+          .filter((key) => key.startsWith('strIngredient'))
+          .map((key) => {
+            const index = key.replace('strIngredient', ''); //대응하는 strMeasureX를 찾기 위해 strIngredient삭제후 번호만 남김
+            const ingredient = meal[key];
+            const measure = meal[`strMeasure${index}`];
+            return {
+              ingredient: ingredient ? ingredient.trim() : '', //null, undefined, 공백이 아닌 경우만 남김
+              measure: measure ? measure.trim() : '',
+            };
+          })
+          .filter((item) => item.ingredient); // 빈 값 제거
+
+        console.log('list of ingredients:', this.ingredients);
       });
     }
   }
